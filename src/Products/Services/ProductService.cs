9c2fa8a00d4f4f57b0305ac07b92d7c0db3e6746
Igor Oliveira <igoroliveira9827@gmail.com>
2024-05-15 20:11:53 -0300

    feat: create new methods for service and fix wrong attribute type in create method

--- src/Products/Services/ProductService.cs
+++ src/Products/Services/ProductService.cs
@@ -1,13 +1,34 @@
 using Products.Interfaces;
 using Products.Model;
+using Npgsql;
+using Products.Dto;
 
 namespace Products.Service;
 
 public class ProductService : IProductService
 {
-    public async Task<object> CreateNewProduct(ProductsModel product)
+    private readonly string _connectionString;
+    public ProductService(IConfiguration configuration)
+    {   
+        _connectionString = configuration.GetConnectionString("ContextString");
+    }
+
+    public async Task<object> CreateNewProduct(ProductsDto product)
     {
-        throw new NotImplementedException();
+        using(NpgsqlConnection connection = new NpgsqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
+            using(var command = new NpgsqlCommand("INSERT INTO product (product_name, product_price, product_quantity) VALUES (@Value01, @Value02, @Value03)", connection))
+            {
+                command.Parameters.AddWithValue("@Value01", product.ProductName);
+                command.Parameters.AddWithValue("@Value02", product.ProductPrice);
+                command.Parameters.AddWithValue("@Value03", product.ProductQtd);
+            
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        return "Product was created!";
     }
 
     public async Task<int> DeleteProduct(int id)
@@ -17,7 +38,29 @@ public class ProductService : IProductService
 
     public async Task<List<ProductsModel>> GetAllProducts()
     {
-        throw new NotImplementedException();
+        List<ProductsModel> products = [];
+
+        using (NpgsqlConnection connection = new(_connectionString))
+        {
+            await connection.OpenAsync();
+
+            using (var command = new NpgsqlCommand("SELECT * FROM product", connection))
+            using (var reader = command.ExecuteReader())
+            {
+                while (await reader.ReadAsync())
+                {
+                    products.Add(new ProductsModel
+                    {
+                        ProductId = (int)reader["product_id"],
+                        ProductName = reader["product_name"].ToString(),
+                        ProductPrice = (double)reader["product_price"],
+                        ProductQtd = (int)reader["product_quantity"]
+                    });
+                }
+            }
+        };
+
+        return products;
     }
 
     public async Task<int> UpdateProduct(int id, ProductsModel product)
