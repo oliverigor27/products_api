650cad8cc4f956793aafec869bb0df93642a70c3
Igor Oliveira <igoroliveira9827@gmail.com>
2024-05-15 18:20:43 -0300

    feat: create a new product model

---
+++ src/Products/Model/ProductsModel.cs
@@ -0,0 +1,8 @@
+namespace Products.Model;
+
+public class ProductsModel
+{
+    public string ProductName { get; set; } = string.Empty;
+    public float Price { get; set; }
+    public int ProductQtd { get; set; }
+}
