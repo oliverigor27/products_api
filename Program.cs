b4fc759cce40fdfd66bed6c5d3679416bdd77a0c
Igor Oliveira <igoroliveira9827@gmail.com>
2024-05-15 18:22:13 -0300

    refactor: refactor the controller use in program archive

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
+using Products.Interfaces;
+using Products.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddController();
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
